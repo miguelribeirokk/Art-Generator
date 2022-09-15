28d95249f004d9da8ca3786defa0210028a79ed0
Miguel_Ribeiro <95700713+miguelribeirokk@users.noreply.github.com>
2022-09-15 18:58:25 -0300

    finalizado

---
+++ tp0/tp0/funcoes.h
@@ -0,0 +1,19 @@
+
+
+
+void menu();
+void flush_in();
+void Asterisco_Simples(int matriz[17][77], int opcao2);
+void Soma_Asterisco(int matriz[17][77], int opcao2);
+void X_Asterisco(int matriz[17][77], int opcao2);
+void Figuras_Aleatorias(int matriz[17][77], int opcao2);
+void Arte_Colorida(int matriz[17][77], int opcao2);
+void Red();
+void Green();
+void White();
+void Pink();
+void Blue();
+void Yellow();
+void Cyan();
+void Criar_Matriz(int matriz[17][77]);
+void Printar_Matriz(int matriz[17][77]);
